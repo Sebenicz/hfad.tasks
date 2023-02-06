2818cc9022dba7975244b6e9016263bbee7e639e
sebastianstrycharz <strycharz97@gmail.com>
2023-02-06 16:38:23 +0100

    feat(viewmodels) bussines logic

--- app/src/main/java/com/hfad/tasks/TaskDao.kt
+++ app/src/main/java/com/hfad/tasks/TaskDao.kt
@@ -6,13 +6,13 @@ import androidx.room.*
 @Dao
 interface TaskDao {
     @Insert
-    fun insert(task: Task)
+    suspend fun insert(task: Task)
     @Update
-    fun update(task: Task)
+    suspend fun update(task: Task)
     @Delete
-    fun delete(task: Task)
-    @Query("SELECT * FROM task_table WHERE taskID = :taskID")
-    fun get(taksId: Long): LiveData<Task>
+    suspend fun delete(task: Task)
+    @Query("SELECT * FROM task_table WHERE taskID = :taskId")
+    fun get(taskId: Long): LiveData<Task>
     @Query("SELECT * FROM task_table ORDER BY taskId DESC")
     fun getAll(): LiveData<List<Task>>
 
--- app/src/main/java/com/hfad/tasks/TasksFragment.kt
+++ app/src/main/java/com/hfad/tasks/TasksFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.hfad.tasks.databinding.FragmentTasksBinding
 
 class TasksFragment : Fragment() {
@@ -17,6 +18,13 @@ class TasksFragment : Fragment() {
     ): View? {
         _binding = FragmentTasksBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        val application = requireNotNull(this.activity).application
+        val dao = TaskDatabase.getInstance(application).taskDAo
+        val viewModelFactory = TaskViewModelFactory(dao)
+        val viewModel = ViewModelProvider(this, viewModelFactory).get(TaskViewModel::class.java)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         return view
     }
 
